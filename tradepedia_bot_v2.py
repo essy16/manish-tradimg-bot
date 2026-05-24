894c91240666076ce3858fa11047bbab138e9fe7
essy <essyc14@gmail.com>
2026-05-24 09:47:45 +0300

    updated so that on weekdays no messages

--- tradepedia_bot_v2.py
+++ tradepedia_bot_v2.py
@@ -1936,7 +1936,7 @@ def schedule_free_channel_posts(context: ContextTypes.DEFAULT_TYPE) -> None:
 
     # Monday-Friday at 10:45 AM UAE
     context.job_queue.run_daily(
-        post_daily_free_channel_update,
+        check_and_post_free_channel_update,
         time=time(hour=10, minute=45, tzinfo=dubai),
         days=(0, 1, 2, 3, 4),
         name="free_channel_morning",
